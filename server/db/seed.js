9377260ffb667fb696c0dc141364f23e48a45c60
19hlj25 <19hlj25@proton.me>
2026-04-06 20:15:45 -0600

    fixed seed script

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,13 +1,19 @@
-import db from ".client";
+import db from "./client.js";
+
+async function seed() {
+  const SQL = `
+    INSERT INTO greetings (message) 
+    VALUES ($1) 
+    RETURNING *;
+  `;
+    
+  const { rows: [message] } = await db.query(SQL, ["Hello World"]);
+  
+  console.log(message);
+}
 
 await db.connect();
 await seed();
 await db.end();
-console.log("database seeded")
 
-async function seed (){
-    const SQL = " INSERT INTO greetings (message) VALUES ($1) RETURNING *
-    
-   const {rows: [message]
-,} = awai9t db.query(SQL, ["Hello World"])
-console.log(row)}
+console.log("database seeded");
